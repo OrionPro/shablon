bbaad1928ba418360e67dcc6315b478d7b78bd72
orionpro <orion79@rambler.ru>
2017-02-04 19:47:43 +0200

    20 00 04.02.2017 Signed-off-by: orionpro <orion79@rambler.ru>

--- js/animate.js
+++ js/animate.js
@@ -1,11 +1,39 @@
 
 // подключение animate.js
 
+
+// Глобальные переменные тайм-линий 
+var tl = new TimelineMax();
+
+function allAnimations () {
+
+	tl.pause();
+
+	// активация тайм линий при загрузке с измерением скролла
+	function readyTimeLineGo() {
+		if ($(window).scrollTop() > 0) {
+			tl.resume();
+		}
+	}
+
+}
+
 $(document).ready(function () {
+
+	if (window.matchMedia("(min-width: 992px)").matches) {
+		allAnimations(); //вся наша анимация greensock
+	}
+
 	// анимации - animate.css
     if (window.matchMedia("(min-width: 768px)").matches) {
 
     }
+
+    readyTimeLineGo();
+	// для IE вызов по скроллу в ready (загруженном документе)
+	$(window).scroll(function () {
+		readyTimeLineGo();
+	});
 
 
 });
