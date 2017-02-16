137ff2193878c3ba1e7698fd2e2d2493260c3987
orionpro <orion79@rambler.ru>
2017-02-16 12:40:59 +0200

    Добавил назад вейпоинт.

--- gulpfile.js
+++ gulpfile.js
@@ -67,6 +67,7 @@ gulp.task('libs', function() {
 		// 'app/libs/modernizr/modernizr.js',
 		'app/libs/jquery/jquery-1.12.4.min.js',
 		'app/libs/jquery/jquery-ui.min.js',
+		'app/libs/waypoints/waypoints.min.js',
 		'app/libs/GreenSock-JS/TweenMax.min.js',
 		'app/libs/GreenSock-JS/DrawSVGPlugin.min.js',
 		'app/libs/animate/animate-css.js',
