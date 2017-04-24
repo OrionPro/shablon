ffb8468d62ed84205cb7a07cae5afaf2c6b305c7
orion <orion79@rambler.ru>
2017-04-24 14:07:50 +0300

    Добавил определение типа браузера в common.js

--- js/common.js
+++ js/common.js
@@ -3,7 +3,29 @@
 
 $(document).ready( function() {
     
-    
+    // Определения браузера
+	function get_name_browser() {
+		// получаем данные userAgent
+		var ua = navigator.userAgent;
+		// с помощью регулярок проверяем наличие текста,
+		// соответствующие тому или иному браузеру
+		if (ua.search(/Chrome/) > 0) return 'Google Chrome';
+		if (ua.search(/Firefox/) > 0) return 'Firefox';
+		if (ua.search(/Opera/) > 0) return 'Opera';
+		if (ua.search(/Safari/) > 0) return 'Safari';
+		if (ua.search(/MSIE/) > 0) return 'Internet Explorer';
+		if (ua.search(/Trident/) > 0) return 'Trident';
+		// условий может быть и больше.
+		// сейчас сделаны проверки только
+		// для популярных браузеров
+		return 'Не определен';
+	}
+
+	if (get_name_browser() == "Trident" || get_name_browser() == "Internet Explorer" || get_name_browser() == "Firefox") {
+		// $(".from_what_is_seo .from_what_is_seo_bot_decor svg").css("bottom", "-217px");
+		// $(".website_promotion .website_promotion_decor").css("bottom", "-177px");
+		// $(".cost_of_online_store .cost_of_online_store_links_item").css("margin-right", "72px");
+	}	
 
     // для инициализации tooltips
     // $( document ).tooltip({
